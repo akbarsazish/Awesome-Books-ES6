aa5f44509f4b254f8374ca9dd7d8fe9f64fb07dc
Luis Renan Pavanello <luisrpavanello@gmail.com>
2023-06-06 19:34:48 -0400

    Update Js.script

--- assets/script.js
+++ assets/script.js
@@ -1,58 +1,61 @@
-  // get book collection from localStorage, or initialize an empty array
-  let books = JSON.parse(localStorage.getItem("books")) || [];
+// get book collection from localStorage, or initialize an empty array
+let books = JSON.parse(localStorage.getItem('books')) || [];
 
-  // Display all books 
-  function displayBooks() {
-    const bookList = document.getElementById("bookList");
-    bookList.innerHTML = "";
+// Display all books
+function displayBooks() {
+  const bookList = document.getElementById('bookList');
+  bookList.innerHTML = '';
 
-    books.forEach(function(book) {
-      const removeBtn = document.createElement("button");
-      const tr = document.createElement("tr");
-       tr.innerHTML = `<td> ${book.title} </td>
-                       <td>  ${book.author} </td> 
-                       `
+  books.forEach((book) => {
+    const removeBtn = document.createElement('button');
+    const tr = document.createElement('tr');
+    tr.innerHTML = `
+      <td>${book.title}</td>
+      <td>${book.author}</td>
+    `;
 
-      removeBtn.textContent = "Remove";
-      removeBtn.classList.add("button");
-      removeBtn.onclick = function() {
-        removeBook(book);
-      };
+    removeBtn.textContent = 'Remove';
+    removeBtn.classList.add('button');
+    removeBtn.onclick = () => {
+    // eslint-disable-next-line no-use-before-define
+      removeBook(book);
+    };
 
-      tr.appendChild(removeBtn);
-      bookList.appendChild(tr);
-    });
-  }
-
+    tr.appendChild(removeBtn);
+    bookList.appendChild(tr);
+  });
+}
 
 // Add a new book
+// eslint-disable-next-line no-unused-vars
 function addBook() {
-  const titleInput = document.getElementById("title");
-  const authorInput = document.getElementById("author");
+  const titleInput = document.getElementById('title');
+  const authorInput = document.getElementById('author');
   const title = titleInput.value.trim();
   const author = authorInput.value.trim();
 
   if (title && author) {
-    const book = { title: title, author: author };
+    const book = { title, author };
     books.push(book);
-    localStorage.setItem("books", JSON.stringify(books));
+    localStorage.setItem('books', JSON.stringify(books));
     displayBooks();
 
     // Clear input fields
-    titleInput.value = "";
-    authorInput.value = "";
+    titleInput.value = '';
+    authorInput.value = '';
   }
 }
 
 // Remove a book from the collection
 function removeBook(bookToRemove) {
-  books = books.filter(function(book) {
-    return book !== bookToRemove;
-  });
+  books = books.filter((book) => book !== bookToRemove);
 
-  localStorage.setItem("books", JSON.stringify(books));
+  localStorage.setItem('books', JSON.stringify(books));
   displayBooks();
 }
 
-  // Initial display of books
-  displayBooks();
+// Initial display of books
+displayBooks();
+
+// eslint-disable-next-line no-unused-vars
+const unusedVariable = 'example';
